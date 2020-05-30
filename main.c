e8071ce45f3886683b15616c17096a3524d59b56
Juraj Borza <juraj.borza@nike.sk>
2020-05-30 17:42:25 +0200

    editline support

--- main.c
+++ main.c
@@ -1,8 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// input buffer
-static char input[2048];
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+char *readline(char* prompt){
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* output = malloc(strlen(buffer)+1);
+  strcpy(outout, buffer);
+  //terminate the output string
+  output[strlen(output)+1) = '\0';
+  return output;
+}
+
+// fake add_history function
+void add_history(char* history) {}
+#else
+// include editline headers for editline
+#include <editline/readline.h>
+#ifdef __linux__
+#include <editline/history.h>
+#endif
+#endif  
 
 int main(int argc, char **argv) {
   // print version information
@@ -11,10 +33,12 @@ int main(int argc, char **argv) {
 
   // REPL
   while (1) {
-    fputs(" > ", stdout);
-    // read input line
-    fgets(input, 2048, stdin);
+    char* input = readline(" > ");
+    add_history(input);
+
     // echo
-    printf("%s", input);
+    printf("%s \n", input);
+
+    free(input);
   }
 }
