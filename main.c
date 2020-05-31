13b4c39bbfe6fb7e9744f1c4c6e9884fe6683d1c
Juraj Borza <jborza@gmail.com>
2020-05-31 10:12:14 +0200

    mpc chapter 6

--- main.c
+++ main.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv) {
   mpc_parser_t *Operator = mpc_new("operator");
   mpc_parser_t *Expr = mpc_new("expr");
   mpc_parser_t *Lispy = mpc_new("lispy");
-
+  // define parsers
   mpca_lang(MPCA_LANG_DEFAULT,
             "                                                     \
           number   : /-?[0-9]+/ ;                             \
@@ -53,7 +53,17 @@ int main(int argc, char **argv) {
     add_history(input);
 
     // echo
-    printf("%s \n", input);
+    // printf("%s \n", input);
+
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      // print AST on success
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
 
     // clean up
     mpc_cleanup(4, Number, Operator, Expr, Lispy);
